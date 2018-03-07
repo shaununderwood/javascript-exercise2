9cf8fb57d98157293cb4d7c58319a59578857c9f
Shaun Underwood <su@sunderwood.co.uk>
2018-03-07 18:55:02 +0100

    updated package.json remove troublesome middleware

--- src/server/server.js
+++ src/server/server.js
@@ -3,7 +3,7 @@ require('babel-register')
 var dispatcher = require('redux-scuttlebutt/lib/server').default;
 
 var webpack = require('webpack');
-var webpackDevMiddleware = require('webpack-dev-middleware');
+// var webpackDevMiddleware = require('webpack-dev-middleware');
 var webpackHotMiddleware = require('webpack-hot-middleware');
 var config = require('./webpack.config');
 
@@ -13,7 +13,7 @@ var app = new (require('express'))();
 var server = require('http').Server(app);
 
 var compiler = webpack(config);
-app.use(webpackDevMiddleware(compiler, { noInfo: true, publicPath: config.output.publicPath }));
+// app.use(webpackDevMiddleware(compiler, { noInfo: true, publicPath: config.output.publicPath }));
 app.use(webpackHotMiddleware(compiler));
 
 app.get("/", function (req, res) {
